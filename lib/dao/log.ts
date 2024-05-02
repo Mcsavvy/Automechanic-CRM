6b76ca8c19a35ee3ba04859675a523bf85676fae
John David <davemcsavvii@gmail.com>
2024-05-02 20:28:49 +0000

    feat: Add essential Log Db operations

---
+++ lib/dao/log.ts
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+import Log from '../models/log';
+
+interface logParams {
+    description: string;
+    target: string;
+    details?: { [key: string]: any};
+    targetId: mongoose.Types.ObjectId;
+    loggerId: mongoose.Types.ObjectId;
+}
+
+async function logCreation(params: logParams) {
+    const log = new Log({...params, action: 'create'});
+    return await log.save();
+}
+
+async function logModification(params: logParams) {
+    const log = new Log({...params, action: 'update'});
+    return await log.save();
+}
+
+async function logDeletion(params: logParams) {
+    const log = new Log({...params, action: 'delete'});
+    return await log.save();
+}
+
+const LogDAO = {
+    logCreation,
+    logModification,
+    logDeletion,
+};
+
+export default LogDAO;
