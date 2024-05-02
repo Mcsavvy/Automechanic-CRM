e5770559db16d85a2a74b0a37aa6c0bd1393f375
John David <davemcsavvii@gmail.com>
2024-05-02 18:04:07 +0000

    refactor: Remove uniqueness from email
    If we aren't actually deleting anything, then the
    uniqueness would pose a problem because we won't
    be able to create another user with that same
    email even though the user using that email has
    been "deleted".

--- lib/models/user.ts
+++ lib/models/user.ts
@@ -13,7 +13,7 @@ const UserSchema = getBaseSchema().add({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   phone: { type: String, required: false },
-  email: { type: String, required: true, unique: true},
+  email: { type: String, required: true},
   password: { type: String, required: true },
 });
 
