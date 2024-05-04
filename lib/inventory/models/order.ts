4b8d95058686fbaf184f1ce59884aabb87120a48
Ugwuanyi Afam <111662034+2022phyro@users.noreply.github.com>
2024-05-04 12:34:20 +0000

    chore: added the buyer dao

--- lib/inventory/models/order.ts
+++ lib/inventory/models/order.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { getBaseSchema, IBaseDocument , defineModel} from '@/lib/common/models/base';
 
 interface IOrderDocument extends IBaseDocument {
-  status: 'pending' | 'cancelled' | 'error' | 'paid';
+  status: 'pending' | 'cancelled' | 'error' | 'rest'| 'paid';
   overdueLimit: Date;
   paymentMethod: 'cash' | 'credit' | 'debit' | 'voucher' | 'bank' | 'cheque';
   customerId: mongoose.Types.ObjectId;
@@ -11,7 +11,7 @@ interface IOrderDocument extends IBaseDocument {
 }
 
 const OrderSchema = getBaseSchema().add({
-    status: { type: String, required: true, enum: ['pending', 'cancelled', 'error', 'paid'] },
+    status: { type: String, required: true, enum: ['pending', 'cancelled', 'error', 'rest', 'paid'], default: 'rest' },
     overdueLimit: { type: Date, required: true },
     paymentMethod: { type: String, required: true, enum: ['cash', 'credit', 'debit', 'voucher', 'bank', 'cheque'] },
     customerId: { type: mongoose.Types.ObjectId, required: true },
