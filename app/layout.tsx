a2d6701c3ac823e0fb8af7dd72fab28a254e6dd9
John David <davemcsavvii@gmail.com>
2024-05-07 10:27:24 +0000

    feat(auth): Add AuthStoreProvider to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
+import { AuthStoreProvider } from "@/lib/providers/auth-store-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ToastContainer position="top-center" hideProgressBar />
-        {children}
+        <AuthStoreProvider>
+          {children}
+        </AuthStoreProvider>
       </body>
     </html>
   );
