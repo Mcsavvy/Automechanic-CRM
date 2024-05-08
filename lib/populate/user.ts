26ed7905f700b6b7ce2c46eb15477dc23e06ca15
John David <davemcsavvii@gmail.com>
2024-05-08 14:55:55 +0000

    refactor: Refactor user population to use UserDAO

--- lib/populate/user.ts
+++ lib/populate/user.ts
@@ -1,16 +1,18 @@
 import User from '../common/models/user';
 import { faker } from '@faker-js/faker';
+import UserDAO from '../common/dao/user';
 
 export default async function populateUsers(number: number) {
     const users = [];
     for (let i = 0; i < number; i++) {
-        const user = new User({
+        const email = faker.internet.email();
+        const user = await UserDAO.addUser({
+            email,
+            password: email,
             firstName: faker.person.firstName(),
             lastName: faker.person.lastName(),
-            email: faker.internet.email(),
-            phoneNumber: faker.phone.number()
+            phone: faker.phone.number(),
         });
-        await user.save();
         users.push(user);
     }
     return users;
