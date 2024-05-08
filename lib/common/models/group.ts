0f359b1a8112722e568b044c1af31bb2bcf4a4f0
John David <davemcsavvii@gmail.com>
2024-05-08 21:21:27 +0000

    refactor: Refactor model to include permission checks

--- lib/common/models/group.ts
+++ lib/common/models/group.ts
@@ -9,16 +9,42 @@ export interface IGroupDocument extends IBaseDocument {
     permissions: {
         [key: string]: string[] | boolean;
     };
+    hasPermission(permission: string): boolean;
+    hasMember(userId: mongoose.Types.ObjectId): boolean;
 }
 
-export const Group = getBaseSchema().add({
-    name: { type: String, required: true },
+export const GroupSchema = getBaseSchema().add({
+    name: { type: String, required: true, unique: true},
     description: { type: String, required: false, default: '' },
     members_ids: { type: [mongoose.Types.ObjectId], required: true, default: [], ref: User },
     permissions: { type: Object, required: true, default: {} },
 });
 
+GroupSchema.methods.hasPermission = function (permission: string) {
+    // example: 'user:read,write'
+    if (permission.includes(":")) {
+        const [scope, action] = permission.split(":", 2);
+        const actions = action.split(",");
+        if (actions.length > 1) {
+            return actions.every((action) =>
+                this.hasPermission(`${scope}:${action}`)
+            );
+        }
+        if (action === "*" || action === "all" || action === "") {
+            return this.permissions[scope] === true;
+        }
+        return (
+            this.permissions[scope] === true ||
+            this.permissions[scope].includes(action)
+        );
+    }
+    return this.permissions[permission] === true;
+};
+
+GroupSchema.methods.hasMember = function (userId: mongoose.Types.ObjectId) {
+    return this.members_ids.includes(userId);
+};
 
 
 
-export default defineModel<IGroupDocument>("Group", Group);
+export default defineModel<IGroupDocument>("Group", GroupSchema);
--- lib/common/models/user.ts
+++ lib/common/models/user.ts
@@ -1,15 +1,17 @@
 import { getBaseSchema, IBaseDocument, defineModel } from './base';
 
-type Role = 'admin' | 'mechanic' | 'teller' | 'owner';
 export interface IUserDocument extends IBaseDocument {
   firstName: string;
   lastName: string;
   phone: string;
   email: string;
   password: string;
-  roles: Role[];
   status: 'active' | 'banned';
+  permissions: {
+    [key: string]: string[] | boolean;
+  };
   fullName(): string;
+  hasPermission(permission: string): boolean;
 }
 
 export const UserSchema = getBaseSchema().add({
@@ -18,12 +20,28 @@ export const UserSchema = getBaseSchema().add({
   phone: { type: String, required: false },
   email: { type: String, required: true},
   password: { type: String, required: true },
+  permissions: { type: Object, required: true, default: {} },
   status: { type: String, required: true, enum: ['active', 'banned'], default: 'active' },
-  roles: { type: [String], required: true, default: ['teller'] }
 });
 
 UserSchema.methods.fullName = function() {
   return this.firstName + ' ' + this.lastName;
+}
+
+UserSchema.methods.hasPermission = function(permission: string) {
+  // example: 'user:read,write'
+  if (permission.includes(':')) {
+    const [scope, action] = permission.split(':', 2);
+    const actions = action.split(',');
+    if (actions.length > 1) {
+      return actions.every(action => this.hasPermission(`${scope}:${action}`));
+    }
+    if (action === '*' || action === 'all' || action === '') {
+      return this.permissions[scope] === true;
+    }
+    return this.permissions[scope] === true || this.permissions[scope].includes(action);
+  }
+  return this.permissions[permission] === true;
 }
 
 export default defineModel<IUserDocument>("User", UserSchema);
