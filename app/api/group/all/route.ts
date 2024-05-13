1af6c5a4d296a9ceb843cddb2c0354415912e10d
Isaac Olumide Ogunfolaju <zicogunfolaju@gmail.com>
2024-05-13 17:23:06 +0000

    feat(api): Add route for listing all groups

---
+++ app/api/group/all/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import permissionRequired from "@/lib/decorators/permission";
+import { Permission } from "@/lib/permissions/base";
+import Group from "@/lib/common/models/group";
+
+interface GroupResponse {
+  id: string;
+  name: string;
+  permissions: {
+    [scope: string]: string[] | boolean;
+  };
+  isMember: boolean;
+}
+
+export const GET = permissionRequired(Permission.AllowAny())(async function (
+  req: NextRequest
+) {
+  const user = this.user;
+  const response: GroupResponse[] = (await Group.find()).map((group) => ({
+    id: group.id,
+    name: group.name,
+    permissions: group.permissions,
+    isMember: group.hasMember(user.id),
+  }));
+  return NextResponse.json(response);
+});
