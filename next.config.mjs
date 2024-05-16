4891306288494837f2eef5cc3d411ce00ff498ad
Isaac Olumide Ogunfolaju <zicogunfolaju@gmail.com>
2024-05-16 17:23:36 +0000

    chore: Removed mongo connection logic from next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-import mongoose from 'mongoose';
-
-const dbUri = process.env.MONGODB_URI;
-if (!globalThis.db) {
-    console.log('Connecting to database:', dbUri);
-    globalThis.db = await mongoose.connect(dbUri);
-
-}
-
-
 const nextConfig = {};
 
 export default nextConfig;
