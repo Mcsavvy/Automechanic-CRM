1faac272633b21e5ed1ce049f55ec8d4c91246d9
John David <davemcsavvii@gmail.com>
2024-05-23 17:06:34 +0000

    refactor: Refactor permissionRequired to handle additional arguments

--- lib/decorators/permission.ts
+++ lib/decorators/permission.ts
@@ -16,31 +16,47 @@ type AuthContext = {
  */
 export default function permissionRequired<T>(permission: Permission) {
     // @ts-ignore
-    return function (handler: T extends (this: infer R, ...args: [NextRequest]) => NextResponse
-        ? (this: R & AuthContext, req: NextRequest) => Promise<NextResponse>
-        : (this: AuthContext, req: NextRequest) => Promise<NextResponse>
+    return function <Args extends any[]>(handler: T extends(this: infer R, ...args: [NextRequest, ...Args]) => NextResponse
+        ? (this: R & AuthContext, ...args: [NextRequest, ...Args]) => Promise<NextResponse>
+        : (this: AuthContext, ...args: [NextRequest, ...Args]) => Promise<NextResponse>
     ){
-        return async function (this: AuthContext | any, req: NextRequest): Promise<NextResponse> {
+        return async function (
+            this: AuthContext | any,
+            ...args: [NextRequest, ...Args]
+        ): Promise<NextResponse> {
             const cookieJar = cookies();
             const token = cookieJar.get("X-Auth-Token")?.value;
+            const req = args[0];
             if (!token) {
-                return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+                return NextResponse.json(
+                    { message: "Unauthorized" },
+                    { status: 401 }
+                );
             }
             // decode the token and check for the required permissions
-            const payload = jwt.decode(token, {json: true});
+            const payload = jwt.decode(token, { json: true });
             if (!payload || !payload.sub) {
-                return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+                return NextResponse.json(
+                    { message: "Unauthorized" },
+                    { status: 401 }
+                );
             }
             const user = await User.findById(payload.sub);
             if (!user) {
-                return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+                return NextResponse.json(
+                    { message: "Unauthorized" },
+                    { status: 401 }
+                );
             }
             const context = this || {};
             context.user = user;
-            if (!await permission.hasPermission(user, req)) {
-                return NextResponse.json({ message: "Insufficient permissions" }, { status: 403 });
+            if (!(await permission.hasPermission(user, req))) {
+                return NextResponse.json(
+                    { message: "Insufficient permissions" },
+                    { status: 403 }
+                );
             }
-            return await handler.call(context, req);
+            return await handler.call(context, ...args);
         };
     };
 }
