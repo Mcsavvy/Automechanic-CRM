9bde30cef6f720894b08c05c39da35e4359b70af
John David <davemcsavvii@gmail.com>
2024-05-28 17:43:31 +0000

    chore Add totalDocs, next, and prev properties to PaginatedDocs interface

--- lib/@types/pagination.ts
+++ lib/@types/pagination.ts
@@ -1,7 +1,10 @@
 export interface PaginatedDocs {
     page: number;
     limit: number;
+    totalDocs: number;
     pageCount: number;
+    next: number | null;
+    prev: number | null;
     hasNextPage: boolean;
     hasPrevPage: boolean;
 }
