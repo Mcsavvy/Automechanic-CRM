666cc518d00ee28efa84a959e5792d5d7725809c
John David <davemcsavvii@gmail.com>
2024-06-04 14:48:37 +0000

    feat: Add orders page with paginationand filters

---
+++ app/inventory/orders/components/date-range.tsx
@@ -0,0 +1,193 @@
+"use client";
+
+import * as React from "react";
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { DateRange } from "react-day-picker";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export type DateRangePreset = {
+  id: string;
+  label: string;
+  apply: (date: DateRange) => DateRange;
+};
+
+interface DateRangeProps
+  extends Omit<React.HTMLAttributes<HTMLDivElement>, "defaultValue"> {
+  max: Date | null;
+  min: Date | null;
+  label: string;
+  defaultValue?: [Date, Date];
+  minState: [Date | null, (value: Date | null) => void];
+  maxState: [Date | null, (value: Date | null) => void];
+  presetsLabel?: string;
+  presets?: DateRangePreset[];
+  handleApply: () => void;
+}
+
+export default function DateRangeFilter({
+  className,
+  label,
+  max,
+  min,
+  minState,
+  maxState,
+  presets,
+  presetsLabel,
+  handleApply,
+}: DateRangeProps) {
+  const [minValue, setMinValue] = minState;
+  const [maxValue, setMaxValue] = maxState;
+  const date: DateRange = {
+    from: minValue || undefined,
+    to: maxValue || undefined,
+  };
+  const initialDate = React.useRef(date);
+  const applyCount = React.useRef(0);
+  const cachedDate = React.useRef(date);
+  const didReset = React.useRef(false);
+
+  const hasPendingChanges = () => {
+    return (
+      date.from !== cachedDate.current.from || date.to !== cachedDate.current.to
+    );
+  };
+
+  const onApply = () => {
+    applyCount.current += 1;
+    cachedDate.current = date;
+    handleApply();
+  };
+
+  const onReset = () => {
+    setMinValue(initialDate.current.from || null);
+    setMaxValue(initialDate.current.to || null);
+    cachedDate.current = initialDate.current;
+    applyCount.current = 0;
+    didReset.current = true;
+  };
+
+  React.useEffect(() => {
+    if (didReset.current) {
+      handleApply();
+    }
+    didReset.current = false;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [minValue, maxValue]);
+
+  function setDate(range?: DateRange) {
+    if (range?.from) setMinValue(range.from);
+    if (range?.to) setMaxValue(range.to);
+  }
+
+  function handleSelect(presetId: string) {
+    const preset = presets?.find((p) => p.id === presetId);
+    if (preset) {
+      setDate({
+        ...date,
+        ...preset.apply(date),
+      });
+    }
+  }
+
+  return (
+    <div className="flex flex-col items-start justify-start mb-2">
+      <div className="flex justify-between items-center mb-2 w-full">
+        <p className="text-sm">{label}</p>
+        <Button
+          variant="ghost"
+          size="sm"
+          className={cn(
+            "text-pri-6 hover:bg-pri-2",
+            hasPendingChanges() ? "" : "hidden"
+          )}
+          onClick={onApply}
+        >
+          Apply
+        </Button>
+        <Button
+          variant="ghost"
+          size="sm"
+          className={cn(
+            "text-acc-6 hover:bg-acc-2",
+            applyCount.current > 0 && !hasPendingChanges()
+              ? ""
+              : "hidden"
+          )}
+          onClick={onReset}
+        >
+          Reset
+        </Button>
+      </div>
+      <div className={cn("grid gap-2", className)}>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              variant={"outline"}
+              className={cn(
+                "w-full justify-start text-left font-normal",
+                !date && "text-muted-foreground"
+              )}
+            >
+              <CalendarIcon className="mr-2 h-4 w-4" />
+              {date?.from ? (
+                date.to ? (
+                  <>
+                    {format(date.from, "LLL dd, y")} -{" "}
+                    {format(date.to, "LLL dd, y")}
+                  </>
+                ) : (
+                  format(date.from, "LLL dd, y")
+                )
+              ) : (
+                <span>{label}</span>
+              )}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent
+            className="flex w-auto flex-col space-y-2 p-2"
+            align="start"
+          >
+            {presets?.length && (
+              <Select onValueChange={handleSelect}>
+                <SelectTrigger>
+                  <SelectValue placeholder={presetsLabel || "Select"} />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  {presets?.map((preset) => (
+                    <SelectItem key={preset.id} value={preset.id}>
+                      {preset.label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            )}
+            <Calendar
+              initialFocus
+              mode="range"
+              defaultMonth={date?.from}
+              selected={date}
+              onSelect={setDate}
+              numberOfMonths={1}
+            />
+          </PopoverContent>
+        </Popover>
+      </div>
+    </div>
+  );
+}
--- app/inventory/orders/page.tsx
+++ app/inventory/orders/page.tsx
@@ -1,72 +1,35 @@
-"use client"
-import { useState } from 'react'
-import { Button } from "@/components/ui/button"
-import { faker } from "@faker-js/faker";
-import { OrderDataTable } from "@/components/ui/ordersDataTable";
-import { Plus } from 'lucide-react'
+"use client";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { DataTable } from "./components/data-table";
+import { useOrderStore } from "@/lib/providers/order-store-provider";
 
-
-const tableHeaders = [
-  { id: 'id', name: "Order ID", isVisible: true, isPresent: true },
-  { id: 'createdAt', name: "Ordered On", isVisible: true },
-  { id: 'buyerId', name: "Ordered By", isVisible: true },
-  { id: 'paymentMethod', name: "Payment Method", isVisible: true },
-  { id: 'isOverdue', name: "OverDue limit", isVisible: true },
-  { id: 'total', name: "Order total", isVisible: true },
-  { id: 'status', name: "Status", isVisible: true },
-];
-const dataFilters = [
-  { id: 'id', name: "Order ID", type: "text" },
-  { id: 'buyerId', name: "Buyer", type: "text"},
-  { id: 'status', name: "Status", type: "text" },
-  { id: 'Before', name: "Before", type: "date" },
-  { id: 'After', name: "After", type: "date" }
-]
-export default function Orders() {
-  const generateFakeData = () => {
-    const data = [];
-    for (let i = 0; i < 10; i++) {
-      const buyerId = faker.person.fullName();
-      const id = faker.string.uuid().slice(1,12);
-      const createdAt = faker.date.recent();
-      const isOverdue: boolean = faker.date.recent() < createdAt;
-      const total = Math.floor(parseFloat(faker.commerce.price()) * Math.random() * 100);
-      const status = ["paid", "pending", "cancelled", "error"][Math.random() * 4 | 0];
-      const paymentMethod = ["bank", "cheque", "cash", "voucher"][Math.random() * 4 | 0];
-      data.push({
-        id,
-        buyerId,
-        createdAt : createdAt.toLocaleDateString(),
-        isOverdue,
-        total,
-        paymentMethod,
-        status,
-      });
-    }
-    return data;
-  };
+export default function Home() {
+  const { orders, page, setPage, pageCount, hasNextPage, hasPrevPage, status } =
+    useOrderStore((state) => state);
   return (
     <>
-      <div className="flex flex-col relative w-full">
+      <div className="flex flex-col absolute h-[calc(100vh-60px)] top-[60px] w-full">
         <div className="h-full relative">
-          <div className="flex flex-row justify-between items-center p-[20px]">
-            <a href="#goods/new"><Button className="flex flex-row gap-2"><Plus size={20} strokeWidth={1.5} />Create new order</Button></a>
+          <div className="flex flex-row justify-between items-center p-4 px-[30px]">
+            <h1 className="text-xl font-heading font-semibold">Orders</h1>
+            <a href="#actions/order/create">
+              <Button className="flex flex-row gap-2">
+                <Plus size={20} strokeWidth={1.5} />
+                New Order
+              </Button>
+            </a>
           </div>
-          <div className="flex flex-row flex-wrap gap-[30px] px-[30px] items-center justify-evenly">
-            <div className=" min-w-[300px] bg-white p-4 flex grow-1 flex-col rounded-md w-[30%] gap-3 border border-pri-3 shadow-inner">
-              <h3 className="text-2xl text-green-500 font-normal font-heading">Completed Orders</h3>
-              <p className="text-lg text-neu-8">Total: 117 </p>
-            </div>
-            <div className=" min-w-[300px] bg-white p-4 flex grow-1 flex-col rounded-md w-[30%] gap-3 border border-pri-3 shadow-inner">
-              <h3 className="text-2xl text-pri-5 font-normal font-heading">Pending Orders</h3>
-              <p className="text-lg text-neu-8">Total: 45</p>
-            </div>
-            <div className=" min-w-[300px] bg-white p-4 flex grow-1 flex-col rounded-md w-[30%] gap-3 border border-pri-3 shadow-inner">
-              <h3 className="text-2xl text-red-500 font-heading font-normal">Errors</h3>
-              <p className="text-lg text-neu-8">Total: 34</p>
-            </div>
-          </div>
-          <OrderDataTable data={generateFakeData()} headers={tableHeaders} filters={dataFilters} />
+          <DataTable
+            data={orders}
+            page={page}
+            status={status}
+            pageCount={pageCount}
+            hasNextPage={hasNextPage}
+            hasPrevPage={hasPrevPage}
+            onNext={() => setPage(page + 1)}
+            onPrev={() => setPage(page - 1)}
+          />
         </div>
       </div>
     </>
