41826034bb44a143bb03b0f97a589c177c1cf560
2022phyro <phyrokelstein2@gmail.com>
2024-07-03 12:02:16 +0100

    Added responsiveness for the recents tab in the dasboard view

--- app/inventory/components/recent-actions.tsx
+++ app/inventory/components/recent-actions.tsx
@@ -1,12 +1,13 @@
 import Overdue from './overdue-order';
 import Recents from './recents'
 import { Button } from '@/components/ui/button'
-import { Boxes, Users, ReceiptText, UsersRound, CircleDollarSign, ShoppingCart, TrendingUp, TrendingDown, LoaderPinwheel } from 'lucide-react'
+import { Boxes, Users, ReceiptText, UsersRound, CircleDollarSign, ShoppingCart, TrendingUp, TrendingDown, LoaderPinwheel, ChevronDown, ChevronRight } from 'lucide-react'
 import Link from 'next/link'
 import { FC, useState, useEffect } from 'react'
 import axios from 'axios';
 import { formatCurrencyShort, formatPercentage } from '@/lib/utils';
 import { DashboardProps } from '@/lib/@types/dashboard';
+import { useWindowWidth } from '../hooks/useWindowWidth'
 
 interface ProductVal {
     name: string;
@@ -21,6 +22,10 @@ const RecentActions: FC<Partial<DashboardProps>> = ({ before, after }) => {
     const [mvp, setMVP] = useState<ProductVal | null>(null)
     const [search, setS] = useState(false)
     const [mpp, setMPP] = useState<ProductVal | null>(null)
+    const [clicked, setClicked] = useState(false)
+    const windowWidth = useWindowWidth()
+
+    const isLargeScreen = windowWidth > 768
     const ctab = [
         { front: "New invoice", icon: ReceiptText },
         { front: "Add new customer", icon: UsersRound },
@@ -53,16 +58,11 @@ const RecentActions: FC<Partial<DashboardProps>> = ({ before, after }) => {
             })
     }, [before, after])
     useEffect(() => {
-        console.log("Summary", mvp) 
+        console.log("Summary", mvp)
     })
     return (
-        <div className="flex flex-row  w-[100%] flex-wrap items-stretch justify-between gap-6 rounded-md">
-            <div className="flex flex-col gap-3 md:w-[60%]">
-                <Recents {...{ before, after }}/>
-                <Overdue/>
-            </div>
-
-            <div className="flex flex-col md: w-[calc(40%-2em)] gap-6 h-full self-stretch">
+        <div className="flex md:flex-row-reverse w-[100%] flex-wrap items-stretch justify-between gap-6 rounded-md">
+            <div className="flex flex-col md:w-[calc(40%-2em)] w-full gap-6 h-full self-stretch">
                 <ul className="flex flex-row flex-wrap items-center justify-start gap-3">
                     {
                         ctab.map(({ front, icon: Icon }, idx) => {
@@ -77,51 +77,65 @@ const RecentActions: FC<Partial<DashboardProps>> = ({ before, after }) => {
                         })
                     }
                 </ul>
-                <div className="rounded-md min-h-[150px] w-full border border-neu-2 bg-white flex flex-col items-center justify-center p-3 gap-3 shadow-md">
-                    <p className="text-sm text-neu-6">Most Valuable Product (MVP)</p>
-                    {search && <LoaderPinwheel size={40} strokeWidth={2.5} color='#2D6DBE' className='animate-spin' />
-                        || (mvp && <Link className='hover:cursor-pointer hover:bg-neu-1 w-full p-2' href={`/inventory/products?query=${mvp.name}`}>
-                            <div className="flex flex-row items-center justify-start gap-4">
-                                <p className="font-semibold text-pri-6 text-lg font-rambla">{mvp.name}</p>
-                                <ul className="grow flex flex-col gap-1">
-                                    <li className="flex flex-row items-center justify-start gap-2"><ShoppingCart strokeWidth={1.5} size={20} /> <span>{mvp.qtySold} sold</span></li>
-                                    <li className="flex flex-row items-center justify-start gap-2"><CircleDollarSign strokeWidth={1.5} size={20} /> <span>{formatCurrencyShort(mvp.revenue)}</span></li>
-                                    <li className="flex flex-row items-center justify-start gap-2"><ReceiptText strokeWidth={1.5} size={20} /> <span>{mvp.orderCount} order{mvp.orderCount > 0 ? 's' : ''}</span></li>
-                                    <li className={` text-${mvp.profitPercentage >= 0 ? 'green-500' : 'red-500'} flex flex-row items-center justify-start gap-2`}>
-                                        {mvp.profitPercentage >= 0 ? <TrendingUp strokeWidth={1.5} size={20} /> : <TrendingDown strokeWidth={1.5} size={20} />}
-                                        <span>{mvp.profitPercentage >= 0 ? '+ ' : '- '}{formatPercentage(mvp.profitPercentage)}</span>
-                                    </li>
-                                </ul>
-                            </div>
-                        </Link>)
-                        || (<div>
-                            <h2 className='text-xl font-bold font-rambla font-acc-7'>No record sales within the period</h2>
-                        </div>)
-                    }
-                </div>
-                <div className="rounded-md min-h-[150px] w-full border border-neu-2 bg-white flex flex-col items-center justify-center p-3 gap-3 shadow-md">
-                    <p className="text-sm text-neu-6">Most Profitable Product (MPP)</p>
-                    {
-                        search && <LoaderPinwheel size={40} strokeWidth={2.5} color='#2D6DBE' className='animate-spin' />
-                        || (mpp && <Link className='hover:cursor-pointer hover:bg-neu-1 w-full p-2' href={`/inventory/products?query=${mpp.name}`}>
-                            <div className="flex flex-row items-center justify-start gap-4">
-                                <p className="font-semibold text-pri-6 text-lg font-rambla">{mpp.name}</p>
-                                <ul className="grow flex flex-col gap-1">
-                                    <li className="flex flex-row items-center justify-start gap-2"><ShoppingCart strokeWidth={1.5} size={20} /> <span>{mpp.qtySold} sold</span></li>
-                                    <li className="flex flex-row items-center justify-start gap-2"><CircleDollarSign strokeWidth={1.5} size={20} /> <span>{formatCurrencyShort(mpp.revenue)}</span></li>
-                                    <li className="flex flex-row items-center justify-start gap-2"><ReceiptText strokeWidth={1.5} size={20} /> <span>{mpp.orderCount} order{mpp.orderCount > 1 ? 's' : ''}</span></li>
-                                    <li className={` text-${mpp.profitPercentage >= 0 ? 'green-500' : 'red-500'} flex flex-row items-center justify-start gap-2`}>
-                                        {mpp.profitPercentage >= 0 ? <TrendingUp strokeWidth={1.5} size={20} /> : <TrendingDown strokeWidth={1.5} size={20} />}
-                                        <span>{mpp.profitPercentage >= 0 ? '+ ' : '- '}{formatPercentage(mpp.profitPercentage)}</span>
-                                    </li>
-                                </ul>
-                            </div>
-                        </Link>)
-                        || (<div>
-                            <h2 className='text-xl font-bold font-rambla font-acc-7'>No record sales within the period</h2>
-                        </div>)
-                    }
-                </div>
+                <h3
+                    className="font-rambla text-lg w-full font-semibold flex flex-row items-center justify-between text-pri-6 pr-4 md:cursor-default md:active:transform-none md:transition-none cursor-pointer transition-transform active:scale-95 md:active:scale-100"
+                    onClick={() => !isLargeScreen && setClicked(!clicked)}
+                >
+                    <span>Your store highlights</span>
+                    {!isLargeScreen && (clicked ? <ChevronDown strokeWidth={1.5} size={24} color={"#003056"} /> : <ChevronRight strokeWidth={1.5} size={24} color={"#003056"} />)}
+                </h3>
+                {(clicked || isLargeScreen) && <>
+                    <div className="rounded-md min-h-[150px] w-full border border-neu-2 bg-white flex flex-col items-center justify-center p-3 gap-3 shadow-md">
+                        <p className="text-sm text-neu-6">Most Valuable Product (MVP)</p>
+                        {search && <LoaderPinwheel size={40} strokeWidth={2.5} color='#2D6DBE' className='animate-spin' />
+                            || (mvp && <Link className='hover:cursor-pointer hover:bg-neu-1 w-full p-2' href={`/inventory/products?query=${mvp.name}`}>
+                                <div className="flex flex-row flex-wrap items-center justify-start gap-4">
+                                    <p className="font-semibold text-pri-6 text-lg font-rambla">{mvp.name}</p>
+                                    <ul className="grow flex flex-col gap-1">
+                                        <li className="flex flex-row items-center justify-start gap-2"><ShoppingCart strokeWidth={1.5} size={20} /> <span>{mvp.qtySold} sold</span></li>
+                                        <li className="flex flex-row items-center justify-start gap-2"><CircleDollarSign strokeWidth={1.5} size={20} /> <span>{formatCurrencyShort(mvp.revenue)}</span></li>
+                                        <li className="flex flex-row items-center justify-start gap-2"><ReceiptText strokeWidth={1.5} size={20} /> <span>{mvp.orderCount} order{mvp.orderCount > 0 ? 's' : ''}</span></li>
+                                        <li className={` text-${mvp.profitPercentage >= 0 ? 'green-500' : 'red-500'} flex flex-row items-center justify-start gap-2`}>
+                                            {mvp.profitPercentage >= 0 ? <TrendingUp strokeWidth={1.5} size={20} /> : <TrendingDown strokeWidth={1.5} size={20} />}
+                                            <span>{mvp.profitPercentage >= 0 ? '+ ' : '- '}{formatPercentage(mvp.profitPercentage)}</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </Link>)
+                            || (<div>
+                                <h2 className='text-xl font-bold font-rambla font-acc-7'>No record sales within the period</h2>
+                            </div>)
+                        }
+                    </div>
+                    <div className="rounded-md min-h-[150px] w-full border border-neu-2 bg-white flex flex-col items-center justify-center p-3 gap-3 shadow-md">
+                        <p className="text-sm text-neu-6">Most Profitable Product (MPP)</p>
+                        {
+                            search && <LoaderPinwheel size={40} strokeWidth={2.5} color='#2D6DBE' className='animate-spin' />
+                            || (mpp && <Link className='hover:cursor-pointer hover:bg-neu-1 w-full p-2' href={`/inventory/products?query=${mpp.name}`}>
+                                <div className="flex flex-row flex-wrap items-center justify-start gap-4">
+                                    <p className="font-semibold text-pri-6 text-lg font-rambla">{mpp.name}</p>
+                                    <ul className="grow flex flex-col gap-1">
+                                        <li className="flex flex-row items-center justify-start gap-2"><ShoppingCart strokeWidth={1.5} size={20} /> <span>{mpp.qtySold} sold</span></li>
+                                        <li className="flex flex-row items-center justify-start gap-2"><CircleDollarSign strokeWidth={1.5} size={20} /> <span>{formatCurrencyShort(mpp.revenue)}</span></li>
+                                        <li className="flex flex-row items-center justify-start gap-2"><ReceiptText strokeWidth={1.5} size={20} /> <span>{mpp.orderCount} order{mpp.orderCount > 1 ? 's' : ''}</span></li>
+                                        <li className={` text-${mpp.profitPercentage >= 0 ? 'green-500' : 'red-500'} flex flex-row items-center justify-start gap-2`}>
+                                            {mpp.profitPercentage >= 0 ? <TrendingUp strokeWidth={1.5} size={20} /> : <TrendingDown strokeWidth={1.5} size={20} />}
+                                            <span>{mpp.profitPercentage >= 0 ? '+ ' : '- '}{formatPercentage(mpp.profitPercentage)}</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </Link>)
+                            || (<div>
+                                <h2 className='text-xl font-bold font-rambla font-acc-7'>No record sales within the period</h2>
+                            </div>)
+                        }
+                    </div>
+                </>
+                }
+            </div>
+            <div className="flex flex-col gap-3 md:w-[60%] w-full">
+                <Recents {...{ before, after }} />
+                <Overdue />
             </div>
         </div>
     )
