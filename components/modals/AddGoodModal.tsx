9390514bf078b27e33a8c037f89e2bb99f8834c2
John David <davemcsavvii@gmail.com>
2024-07-03 16:01:18 +0000

    Update AddGoodModal title to "Add Product"

--- components/modals/AddGoodModal.tsx
+++ components/modals/AddGoodModal.tsx
@@ -192,7 +192,7 @@ const AddGoodModal: FC<AddGoodProps> = () => {
           ref={modalRef}
         >
           <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
-            <h3 className="text-xl font-semibold text-primary">{name}</h3>
+            <h3 className="text-xl font-semibold text-primary">Add Product</h3>
             <button
               type="button"
               className="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
