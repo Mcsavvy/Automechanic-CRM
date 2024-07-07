ce4a7e2fdb4e5726385b2dbb4417a69c83d4a3ae
John David <davemcsavvii@gmail.com>
2024-07-07 01:46:34 +0000

    feat: Add onValueChange prop to NumberInput component
    use this to handle max and min values

--- components/ui/number-input.tsx
+++ components/ui/number-input.tsx
@@ -5,10 +5,26 @@ interface NumberInputProps
   extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "type"> {
   prependSymbol?: boolean;
   symbol: string;
+  onValueChange?: (value: number) => void;
 }
 
 const NumberInput = React.forwardRef<HTMLInputElement, NumberInputProps>(
   ({ prependSymbol = false, symbol, className, ...props }, ref) => {
+    props.onValueChange = props.onValueChange || (() => {});
+  
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+      let value = parseFloat(e.target.value);
+      if (isNaN(value)) {
+        value = 0;
+      }
+      if (props.max && value > (props.max as number)) {
+        value = props.max as number;
+      }
+      if (props.min && value < (props.min as number)) {
+        value = props.min as number;
+      }
+      props.onValueChange!(value);
+    }
     const Symbol = () => (
       <div className="bg-gray-200 text-black text-sm p-2 rounded-l-md border border-input">
         {symbol}
@@ -26,6 +42,7 @@ const NumberInput = React.forwardRef<HTMLInputElement, NumberInputProps>(
           }
           ref={ref}
           {...props}
+          onChange={props.onChange || handleChange}
         />
         {!prependSymbol && <Symbol />}
       </div>
