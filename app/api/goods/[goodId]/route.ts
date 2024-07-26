dc944d6085d739468f6b086519793c05d2d8fbd0
2022phyro <phyrokelstein2@gmail.com>
2024-07-26 19:11:24 +0100

    Added logging to the routes involving goods

--- app/api/goods/[goodId]/route.ts
+++ app/api/goods/[goodId]/route.ts
@@ -2,8 +2,8 @@ import { Permission } from "@/lib/permissions/base";
 import permissionRequired from "@/lib/decorators/permission";
 import { NextResponse } from "next/server";
 import GoodDAO from "@/lib/inventory/dao/good";
-import mongoose from "mongoose";
-
+import LogDAO, { logParams } from "@/lib/common/dao/log";
+import mongoose, { Types } from "mongoose";
 interface UpdateGoodBody {
   name?: string;
   costPrice?: number;
@@ -29,6 +29,17 @@ export const PUT = permissionRequired(Permission.AllowAny())(async function (
   { params }: { params: { goodId: string } }
 ) {
   const body = (await req.json()) as UpdateGoodBody;
+
+  const prevGood = await GoodDAO.getGood(
+    mongoose.Types.ObjectId.createFromHexString(params.goodId)
+  );
+  if (!prevGood) {
+    return NextResponse.json({ message: "Good not found" }, { status: 404 });
+  }
+  const details: { [key: string]: any } = {};
+  for (const key of Object.keys(body) as (keyof UpdateGoodBody)[]) {
+    details[key] = prevGood[key];
+  } 
   const good = await GoodDAO.updateGood(
     mongoose.Types.ObjectId.createFromHexString(params.goodId),
     body
@@ -36,6 +47,14 @@ export const PUT = permissionRequired(Permission.AllowAny())(async function (
   if (!good) {
     return NextResponse.json({ message: "Good not found" }, { status: 404 });
   }
+  const logDetails: logParams = {
+    display: [this.user.fullName(), good.name],
+    targetId: Types.ObjectId.createFromHexString(good.id),
+    loggerId: Types.ObjectId.createFromHexString(this.user.id),
+    target: "Good",
+    details,
+  };
+  await LogDAO.logModification(logDetails);
   return NextResponse.json(good);
 });
 
@@ -46,8 +65,16 @@ export const DELETE = permissionRequired(Permission.AllowAny())(async function (
   const good = await GoodDAO.deleteGood(
     mongoose.Types.ObjectId.createFromHexString(params.goodId)
   );
+
   if (!good) {
     return NextResponse.json({ message: "Good not found" }, { status: 404 });
   }
+  const logDetails: logParams = {
+    display: [this.user.fullName(), good.name],
+    targetId: Types.ObjectId.createFromHexString(good.id),
+    loggerId: Types.ObjectId.createFromHexString(this.user.id),
+    target: "Good",
+  };
+  await LogDAO.logDeletion(logDetails);
   return new NextResponse(null, { status: 204 });
 });
--- app/api/goods/route.ts
+++ app/api/goods/route.ts
@@ -3,6 +3,8 @@ import Good, { IGoodDocument } from "@/lib/inventory/models/good";
 import permissionRequired from "@/lib/decorators/permission";
 import { NextResponse } from "next/server";
 import GoodDAO from "@/lib/inventory/dao/good";
+import LogDAO, { logParams } from "@/lib/common/dao/log";
+import { Types } from "mongoose";
 
 interface CreateGoodBody {
   name: string;
@@ -20,5 +22,12 @@ export const POST = permissionRequired(Permission.AllowAny())(async function (
 ) {
   const body = (await req.json()) as CreateGoodBody;
   const good = await GoodDAO.addGood(body);
+  const logDetails: logParams = {
+    display: [this.user.fullName(), good.name], 
+    targetId: Types.ObjectId.createFromHexString(good.id),
+    loggerId: Types.ObjectId.createFromHexString(this.user.id),
+    target: "Good",
+  }
+  await LogDAO.logCreation(logDetails);
   return NextResponse.json(good, { status: 201 });
 });
