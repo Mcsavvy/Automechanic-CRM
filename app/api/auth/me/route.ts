ada9539d71d6fcd8e698fda9799c5a4c0b9fe0c3
Dave Mcsavvy <davemcsavvii@gmail.com>
2024-09-17 22:32:29 +0100

    refactor: Update user types and auth store
    The user types in the `user.ts` file have been updated to include the `permissions` property. Additionally, the `AuthState` interface in the `auth-store.ts` file has been modified to include the `permissions` and `groups` properties. This ensures that the user's permissions and groups are properly stored and managed in the authentication store.

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,15 +1,33 @@
 import { NextResponse } from "next/server";
 import permissionRequired from "@/lib/decorators/permission";
 import { Permission } from "@/lib/permissions/server";
+import {type User} from "@/lib/@types/user";
+import Group from "@/lib/common/models/group";
 
-export const POST = permissionRequired(Permission.AllowAny())(async function (req) {
-    const user = this.user;
-    const response: AuthResponse = {
-        id: user._id.toString(),
-        email: user.email,
-        lastName: user.lastName,
-        firstName: user.firstName,
-        phone: user.phone
-    };
-    return NextResponse.json(response);
+export const POST = permissionRequired(Permission.AllowAny())(async function (
+  req
+) {
+  const user = this.user;
+  const groups = await Group.find(
+    { members_ids: user._id },
+    {
+      _id: 1,
+      name: 1,
+      permissions: 1,
+    }
+  );
+  const response: User = {
+    id: user._id.toString(),
+    email: user.email,
+    lastName: user.lastName,
+    firstName: user.firstName,
+    phone: user.phone,
+    permissions: user.permissions,
+    groups: groups.map((group) => ({
+      id: group._id.toString(),
+      name: group.name,
+      permissions: group.permissions,
+    })),
+  };
+  return NextResponse.json(response);
 });
