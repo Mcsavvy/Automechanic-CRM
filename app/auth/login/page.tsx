588e7feb449b8aedef4f2bf0c6a504cdc5194339
Dave Mcsavvy <davemcsavvii@gmail.com>
2024-09-18 14:21:23 +0100

    feat: Update login page with new background image and user authentication logic
    The login page has been updated to use a new background image and improve the user authentication logic. The background image has been replaced with a new image for a refreshed look. Additionally, the user authentication logic has been enhanced to handle the login process more securely and efficiently. These changes improve the visual appeal of the login page and enhance the overall user experience.

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,29 +1,23 @@
 "use client";
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import React, { useState } from "react";
-import backgroundImage from "./background.png";
 import { toast } from "react-toastify";
 import axios from "axios";
 import { useAuthStore } from "@/lib/providers/auth-store-provider";
+import { User } from "@/lib/@types/user";
 
-
-interface LoginResponse {
-    id: string;
-    email: string;
-    lastName: string;
-    phone: string;
-    firstName: string;
-}
+interface LoginResponse extends User {}
 
 interface ErrorResponse {
-    message: string;
+  message: string;
 }
 
 const LoginPage: React.FC = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const {loggedIn, setAuth} = useAuthStore((state) => state);
-    const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { loggedIn, setAuth } = useAuthStore((state) => state);
+  const router = useRouter();
+  const query = useSearchParams();
 
   React.useEffect(() => {
     const redirect = query.get("redirect");
@@ -36,92 +30,97 @@ const LoginPage: React.FC = () => {
     }
   }, [loggedIn, router, query]);
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const promise = new Promise<LoginResponse>((resolve, reject) => {
-            axios.post("/api/auth/login", { email, password }).then((response) => {
-                const data = response.data as LoginResponse;
-                setAuth({
-                    id: data.id,
-                    email: data.email,
-                    firstName: data.firstName,
-                    lastName: data.lastName,
-                    phone: data.phone,
-                    loggedIn: true,
-                });
-                resolve(data);
-            }).catch((error) => {
-                reject(error.response.data as ErrorResponse);
-            });
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const promise = new Promise<LoginResponse>((resolve, reject) => {
+      axios
+        .post("/api/auth/login", { email, password })
+        .then((response) => {
+          const data = response.data as LoginResponse;
+          setAuth({
+            id: data.id,
+            email: data.email,
+            firstName: data.firstName,
+            lastName: data.lastName,
+            phone: data.phone,
+            groups: data.groups,
+            permissions: data.permissions,
+            loggedIn: true,
+          });
+          resolve(data);
+        })
+        .catch((error) => {
+          reject(error.response.data as ErrorResponse);
         });
-        toast.promise<LoginResponse,ErrorResponse>(promise, {
-            pending: "Logging in...",
-            success: {
-                render: ({data}) => `Welcome back, ${data.firstName}`,
-            },
-            error: {
-                render: ({data}) => data.message || "An error occurred",
-            },
-        });
-    };
+    });
+    toast.promise<LoginResponse, ErrorResponse>(promise, {
+      pending: "Logging in...",
+      success: {
+        render: ({ data }) => `Welcome back, ${data.firstName}`,
+      },
+      error: {
+        render: ({ data }) => data.message || "An error occurred",
+      },
+    });
+  };
 
-    return (
-        <div
-            className="min-h-screen flex items-center justify-center bg-gray-100"
-            style={{
-                backgroundImage: 'url(/background.png)',
-                backgroundPosition: "center",
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat",
-            }}
-        >
-            <form
-                onSubmit={handleSubmit}
-                className="bg-white p-8 rounded-lg shadow-lg max-w-sm w-full"
-            >
-                <div className="mb-6">
-                    <label
-                        htmlFor="email"
-                        className="block text-sm font-medium text-gray-700"
-                    >
-                        Email
-                    </label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        required
-                        className="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm text-gray-700"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
-                <div className="mb-6">
-                    <label
-                        htmlFor="password"
-                        className="block text-sm font-medium text-gray-700"
-                    >
-                        Password
-                    </label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        required
-                        className="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm text-gray-700"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <button
-                    type="submit"
-                    className="w-full bg-gray-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                >
-                    Log in
-                </button>
-            </form>
+  return (
+    <div
+      className="min-h-screen flex items-center justify-center bg-gray-100"
+      style={{
+        backgroundImage: 'url(/background.png)',
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white p-8 rounded-lg shadow-lg max-w-sm w-full"
+      >
+        <div className="mb-6">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            className="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm text-gray-700"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-    );
+        <div className="mb-6">
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            required
+            className="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm text-gray-700"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-gray-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Log in
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
