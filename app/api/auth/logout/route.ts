2c0b3ae49ea95d1b2a26db754829bdd6a7ee1ec3
Dave Mcsavvy <davemcsavvii@gmail.com>
2024-09-19 18:35:36 +0100

    refactor: Update logout route to use AUTH_COOKIE_NAME constant

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,8 +1,9 @@
 import { NextRequest } from "next/server";
 import { cookies } from "next/headers";
+import { AUTH_COOKIE_NAME } from "@/config";
 
 export async function POST(req: NextRequest) {
-    cookies().delete("X-Auth-Token");
+    cookies().delete(AUTH_COOKIE_NAME);
     return new Response(null, {
         status: 204,
     });
