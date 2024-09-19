fcbdf042bc5e57e367873331ee42af115f7b4d15
Dave Mcsavvy <davemcsavvii@gmail.com>
2024-09-19 18:35:54 +0100

    style: Format user dao

--- lib/common/dao/user.ts
+++ lib/common/dao/user.ts
@@ -1,76 +1,79 @@
 import bcrypt from "bcrypt";
 import { SignJWT, jwtVerify, type JWTPayload } from "jose";
 import UserModel, { IUserDocument } from "../models/user";
-import mongoose, { FilterQuery } from 'mongoose';
+import mongoose, { FilterQuery } from "mongoose";
 import {
-    validateEmail,
-    validateFirstName,
-    validateLastName,
-    validatePassword,
-    validatePhoneNumber,
+  validateEmail,
+  validateFirstName,
+  validateLastName,
+  validatePassword,
+  validatePhoneNumber,
 } from "../validation";
 import LogDAO from "./log";
+import { JWT_EXPIRY, JWT_SECRET } from "@/config";
 
 interface createUserParams {
-    firstName: string;
-    lastName: string;
-    email: string;
-    phone: string;
-    password: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  password: string;
 }
 
 interface updateUserParams {
-    firstName?: string;
-    lastName?: string;
-    email?: string;
-    phone?: string;
-    password?: string;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  phone?: string;
+  password?: string;
 }
 
 interface PaginatedUsers {
-    users: IUserDocument[];
-    totalDocs: number;
-    limit: number;
-    page: number;
-    totalPages: number;
-    next: number | null;
-    prev: number | null;
-    hasPrevPage: boolean;
-    hasNextPage: boolean;
+  users: IUserDocument[];
+  totalDocs: number;
+  limit: number;
+  page: number;
+  totalPages: number;
+  next: number | null;
+  prev: number | null;
+  hasPrevPage: boolean;
+  hasNextPage: boolean;
 }
 
 async function addUser({
+  firstName,
+  lastName,
+  email,
+  phone,
+  password,
+}: createUserParams) {
+  firstName = validateFirstName(firstName);
+  lastName = validateLastName(lastName);
+  email = validateEmail(email);
+  phone = validatePhoneNumber(phone);
+  password = validatePassword(password);
+
+  if (
+    (await UserModel.countDocuments({ email, isDeleted: false }).exec()) > 0
+  ) {
+    throw new Error("User with email already exists");
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new UserModel({
     firstName,
     lastName,
     email,
     phone,
-    password,
-}: createUserParams) {
-    firstName = validateFirstName(firstName);
-    lastName = validateLastName(lastName);
-    email = validateEmail(email);
-    phone = validatePhoneNumber(phone);
-    password = validatePassword(password);
-
-    if ((await UserModel.countDocuments({ email, isDeleted: false }).exec()) > 0) {
-        throw new Error("User with email already exists");
-    }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new UserModel({
-        firstName,
-        lastName,
-        email,
-        phone,
-        password: hashedPassword,
-    });
-    await user.save();
-    LogDAO.logCreation({
-        description: `User ${user.fullName()} created`,
-        target: "User",
-        targetId: user._id,
-        loggerId: user._id,
-    });
-    return user;
+    password: hashedPassword,
+  });
+  await user.save();
+  LogDAO.logCreation({
+    description: `User ${user.fullName()} created`,
+    target: "User",
+    targetId: user._id,
+    loggerId: user._id,
+  });
+  return user;
 }
 
 async function getUsers({
@@ -99,7 +102,7 @@ async function getUsers({
   const users = await UserModel.find(query)
     .skip(skip)
     .limit(limit)
-    .sort({ firstName: 1, lastName: 1})
+    .sort({ firstName: 1, lastName: 1 })
     .lean()
     .exec();
   const next = users.length === limit ? page + 1 : null;
@@ -119,82 +122,88 @@ async function getUsers({
 }
 
 async function updateUser(
-    id: mongoose.Types.ObjectId,
-    params: updateUserParams
+  id: mongoose.Types.ObjectId,
+  params: updateUserParams
 ) {
-    const firstName = params.firstName
-        ? validateFirstName(params.firstName)
-        : undefined;
-    const lastName = params.lastName
-        ? validateLastName(params.lastName)
-        : undefined;
-    const email = params.email ? validateEmail(params.email) : undefined;
-    const phone = params.phone ? validatePhoneNumber(params.phone) : undefined;
-    const password = params.password
-        ? validatePassword(params.password)
-        : undefined;
-    const passwordHash = password ? await bcrypt.hash(password, 10) : undefined;
+  const firstName = params.firstName
+    ? validateFirstName(params.firstName)
+    : undefined;
+  const lastName = params.lastName
+    ? validateLastName(params.lastName)
+    : undefined;
+  const email = params.email ? validateEmail(params.email) : undefined;
+  const phone = params.phone ? validatePhoneNumber(params.phone) : undefined;
+  const password = params.password
+    ? validatePassword(params.password)
+    : undefined;
+  const passwordHash = password ? await bcrypt.hash(password, 10) : undefined;
 
-    if (email && (await UserModel.countDocuments({ email, _id: {"$ne": id}, isDeleted: false }).exec()) > 0) {
-        throw new Error("User with email already exists");
-    }
-    const user = await UserModel.findOne({_id: id, isDeleted: false });
-    if (!user) {
-        throw new Error("User not found");
-    }
-    const details: updateUserParams = {};
-    const payload: any = {};
-    if (firstName){
-         payload.firstName = firstName
-         details.firstName = user.firstName
+  if (
+    email &&
+    (await UserModel.countDocuments({
+      email,
+      _id: { $ne: id },
+      isDeleted: false,
+    }).exec()) > 0
+  ) {
+    throw new Error("User with email already exists");
+  }
+  const user = await UserModel.findOne({ _id: id, isDeleted: false });
+  if (!user) {
+    throw new Error("User not found");
+  }
+  const details: updateUserParams = {};
+  const payload: any = {};
+  if (firstName) {
+    payload.firstName = firstName;
+    details.firstName = user.firstName;
+  }
+  if (lastName) {
+    payload.lastName = lastName;
+    details.lastName = user.lastName;
+  }
+  if (email) {
+    payload.email = email;
+    details.email = user.email;
+  }
+  if (phone) {
+    payload.phone = phone;
+    details.phone = user.phone;
+  }
+  if (passwordHash) {
+    payload.password = passwordHash;
+    details.password = user.password;
+  }
+  if (!payload) return user;
 
-    };
-    if (lastName) {
-        payload.lastName = lastName;
-        details.lastName = user.lastName;
-    }
-    if (email) {
-        payload.email = email;
-        details.email = user.email;
-    }
-    if (phone) {
-        payload.phone = phone;
-        details.phone = user.phone;
-    }
-    if (passwordHash) {
-        payload.password = passwordHash;
-        details.password = user.password
-    }
-    if (!payload) return user;
-
-    Object.assign(user, payload);
-    await user.save();
-    LogDAO.logModification({
-        description: `User ${user.fullName()} updated`,
-        target: "User",
-        targetId: user._id,
-        loggerId: user._id,
-        details,
-    });
-    return user;
+  Object.assign(user, payload);
+  await user.save();
+  LogDAO.logModification({
+    description: `User ${user.fullName()} updated`,
+    target: "User",
+    targetId: user._id,
+    loggerId: user._id,
+    details,
+  });
+  return user;
 }
 
 async function deleteUser(id: mongoose.Types.ObjectId) {
-    const user = await UserModel.findOneAndUpdate(
-        { _id: id, isDeleted: false },
-        { isDeleted: true },
-        { new: true }
-    );
-    if (!user) {
-        throw new Error("User not found");
-    }
-    LogDAO.logDeletion({
-        description: `User ${user.fullName()} deleted`,
-        target: "User",
-        targetId: user._id,
-        loggerId: user._id,
-    });
-    return user;
+  const user = await UserModel.findOneAndUpdate(
+    { _id: id, isDeleted: false },
+    { isDeleted: true },
+    { new: true }
+  );
+  if (!user) {
+    throw new Error("User not found");
+  }
+  LogDAO.logDeletion({
+    description: `User ${user.fullName()} deleted`,
+    target: "User",
+    targetId: user._id,
+    loggerId: user._id,
+  });
+  return user;
 }
 
 async function authenticateUser(email: string, password: string) {
@@ -218,28 +227,34 @@ async function authenticateUser(email: string, password: string) {
 }
 
 async function getUser(id: mongoose.Types.ObjectId) {
-    const user = await UserModel.findOne({ _id: id, isDeleted: false }, { password: 0 , __v: 0, isDeleted: 0}).exec();
-    return user
+  const user = await UserModel.findOne(
+    { _id: id, isDeleted: false },
+    { password: 0, __v: 0, isDeleted: 0 }
+  ).exec();
+  return user;
 }
 
-async function setUserStatus(id: mongoose.Types.ObjectId, status: "banned" | "active") {
-    const user = await UserModel.findOne({ _id: id, isDeleted: false });
-    if (!user) {
-        throw new Error("User not found");
-    }
-    user.status = status;
-    await user.save();
-    return user;
+async function setUserStatus(
+  id: mongoose.Types.ObjectId,
+  status: "banned" | "active"
+) {
+  const user = await UserModel.findOne({ _id: id, isDeleted: false });
+  if (!user) {
+    throw new Error("User not found");
+  }
+  user.status = status;
+  await user.save();
+  return user;
 }
 
 const UserDAO = {
-    addUser,
-    getUsers,
-    deleteUser,
-    updateUser,
-    authenticateUser,
-    getUser,
-    setUserStatus
+  addUser,
+  getUsers,
+  deleteUser,
+  updateUser,
+  authenticateUser,
+  getUser,
+  setUserStatus,
 };
 
 export default UserDAO;
